f2e9ff2294d97e512ec49d286a8115732a3d9a33
Polina <sgsey75@gmail.com>
2021-10-04 22:54:22 +0300

    resolve task 3.2

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let editor = document.getElementById('editor');
+let button = document.querySelector('.button');
+
+editor.oninput = () => {
+    let editorText = editor.value;
+    localStorage.setItem('value', editorText);
+}
+
+window.onload = () => {
+    editor.value = localStorage.getItem('value');
+}
+
+button.addEventListener('click', () => {
+    localStorage.clear('value');
+    editor.value = "";
+})
