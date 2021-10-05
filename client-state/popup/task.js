e4b056bb410f0c69a2e92719f7ff5f18cae45684
Polina <sgsey75@gmail.com>
2021-10-06 02:42:27 +0300

    resolve task 3.2

---
+++ client-state/popup/task.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let subscribeModal = document.getElementById("subscribe-modal");
+let modalClose = document.getElementsByClassName("modal__close"); 
+
+window.onload = () => {
+    if (document.cookie[modalClose] !== true) {
+        subscribeModal.classList.add("modal_active");
+    }
+}
+
+modalClose[0].onclick = function() {
+    document.cookie='modalClose=true';
+    subscribeModal.classList.remove("modal_active");
+}
+
