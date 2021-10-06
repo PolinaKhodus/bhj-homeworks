f3536ec43152cb5362e882252985dbd3b35e127f
Polina <sgsey75@gmail.com>
2021-10-06 15:23:07 +0300

    fix task 3.2

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,45 +1,48 @@
 'use strict'
 
-function mainFunct() {
-    const signinForm = document.getElementById("signin__form");
-    const signin = document.querySelector(".signin");
-    const welcome = document.querySelector(".welcome");
-  
-    function switchingClasses(){
-      signin.classList.remove("signin_active");
-      welcome.classList.add("welcome_active");
-    }
-  
-    if (localStorage.id) {
-      switchingClasses();
-      welcome.textContent += localStorage.id;
-    } 
-  
-    signinForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const formData = new FormData(signinForm);
-      const xhr = new XMLHttpRequest();
-      let url = 'https://netology-slow-rest.herokuapp.com/auth.php';
-
-      xhr.open("POST", url);
-  
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === xhr.DONE && this.status === 200) {
-          const data = JSON.parse(this.responseText);
-          verifAuthoriz(data);
-        }
-      });
-      xhr.send(formData);
-    });
-  
-    function verifAuthoriz(data) {
-      if (data.success) {
+const signinForm = document.getElementById("signin__form");
+const signin = document.querySelector(".signin");
+const welcome = document.querySelector(".welcome");
+    
+function verifyAuthorize(data) {
+    if (data.success) {
         switchingClasses();
         welcome.textContent += data.user_id;
         localStorage.id = data.user_id;
-      } else {
+    } else {
         alert("Неверный логин/пароль");
-      }
     }
-  }
-  mainFunct();
+}
+
+function switchingClasses(){
+    signin.classList.remove("signin_active");
+    welcome.classList.add("welcome_active");
+}
+
+function initAuthorization() {  
+    if (localStorage.id) {
+        switchingClasses();
+        welcome.textContent += localStorage.id;
+    } 
+  
+    signinForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(signinForm);
+        const xhr = new XMLHttpRequest();
+        const urlAuth = "https://netology-slow-rest.herokuapp.com/auth.php";
+
+        xhr.open("POST", urlAuth);
+  
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === xhr.DONE && this.status === 200) {
+                const data = JSON.parse(this.responseText);
+                verifyAuthorize(data);
+            }
+        });
+
+      xhr.send(formData);
+
+    });    
+}
+
+initAuthorization();
